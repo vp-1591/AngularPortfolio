21f426dd51f889dc07fd2c6808643de0e840db30
Vadym <abrosimov.vadym@gmail.com>
2025-11-08 12:49:52 +0100

    feat/about RWD

--- src/app/features/home/about/stat.component.ts
+++ src/app/features/home/about/stat.component.ts
@@ -6,15 +6,16 @@ import { TextComponent } from '../../../shared/ui/text.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <text
+      class="valueText"
       [styles]="{
-        fontSize: '30px',
         color: 'var(--primary-color)',
         textShadow: '0 0 20px var(--primary-color)'
       }"
       >{{ statValue }}</text
     >
     <text
-      [styles]="{ fontSize: '14px', textShadow: '0 0 20px var(--primary-color)' }"
+      class="labelText"
+      [styles]="{ textShadow: '0 0 20px var(--primary-color)' }"
       type="Inter"
       >{{ statLabel }}</text
     >
@@ -33,6 +34,24 @@ import { TextComponent } from '../../../shared/ui/text.component';
         align-items: center;
         min-width: 18vw;
         gap: 2px;
+      }
+      .valueText {
+        font-size: 30px;
+      }
+      .labelText {
+        font-size: 14px;
+      }
+
+      @media (max-width: 768px) {
+        :host {
+          width: 70%;
+        }
+        .valueText {
+          font-size: 24px;
+        }
+        .labelText {
+          font-size: 12px;
+        }
       }
     `,
   ],
